0694cc97f1cd4a138546c38f2513cdc476cd320c
zlatanovcoding <zlatanovcoding@gmail.com>
2022-10-07 10:59:17 +0300

    Update to CondensedArrays

--- Fundamentals/Fundamentals - 3/condenseArray.js
+++ Fundamentals/Fundamentals - 3/condenseArray.js
@@ -1,29 +1,31 @@
 function solve(arr) {
   let condensed = [];
   if (arr.length > 1) {
-  
     for (let i = 0; i < arr.length; i++) {
       if (i < arr.length - 1) {
-      let  sum = arr[i] + arr[i + 1];
-        
-        condensed.push(sum.toString());
+        let sum = arr[i] + arr[i + 1];
+
+        condensed.push(sum);
       }
     }
-    
     let tempArr = [];
-    while(condensed.length >1) {
-      tempArr = Number(condensed);
-      for (let i = 0; i < tempArr.length; i++) {
-          let sum = tempArr[i] + tempArr[i + 1];
-          tempArr.push(sum.toString());
-          condensed = tempArr
-      
+    while (condensed.length > 1) {
+      for (let i = 0; i < condensed.length; i++) {
+        if (i < condensed.length - 1) {
+          let sum = condensed[i] + condensed[i + 1];
+          tempArr.push(sum);
+        }
+      }
+      condensed = tempArr;
+      tempArr = [];
     }
   }
-} else {
+  if (arr.length > 1) {
+    console.log(condensed.join());
+  } else if (arr.length == 1) {
+    console.log(arr.join());
+  } else {
     console.log(`${arr[0]} is already condensed to number`);
   }
-  console.log(arr);
 }
-
 solve([5, 0, 4, 1, 2]);
