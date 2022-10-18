8a6e86b8b08b12153dc1f92d7caeca7eb24feafa
zlatanovcoding <zlatanovcoding@gmail.com>
2022-10-18 14:34:48 +0300

    Update

--- Fundamentals Exercises/Fundamentals Exercises - 3/maxSequence.js
+++ Fundamentals Exercises/Fundamentals Exercises - 3/maxSequence.js
@@ -1,4 +1,33 @@
-function solve() {
-
+function solve(arr) {
+  let counter = 1;
+  let index = 0;
+  let tempCount = 1;
+  let tempIndex = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let g = i + 1; g < arr.length; g++) {
+      if (arr[g] !== arr[i]) {
+        if (tempCount == counter) {
+          tempIndex = index;
+          tempCount= 1;
+          break;
+        }
+        if (tempCount > counter) {
+          counter = tempCount;
+          tempCount = 1;
+          index = tempIndex;
+          tempIndex = 0;
+          break;
+        }
+        break;
+      }
+      tempCount += 1;
+      tempIndex = g;
+    }
+    if (i + 1 >= arr.length && tempCount > counter) {
+      counter = tempCount;
+    }
+  }
+  let result = String(arr[index]) + " ";
+  console.log(`${result.repeat(counter)}`);
 }
-solve()
+solve([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
