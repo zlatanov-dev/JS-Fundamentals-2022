b67c75b08c5c25d498a2efb40af788443110dc85
zlatanovcoding <zlatanovcoding@gmail.com>
2022-10-18 19:59:23 +0300

    Update of previous exercise

--- Fundamentals Exercises/Fundamentals Exercises - 3/magicSum.js
+++ Fundamentals Exercises/Fundamentals Exercises - 3/magicSum.js
@@ -1,4 +1,13 @@
-function solve() {
-    
+function solve(arr, magicNumber) {
+    for(let i = 0; i<arr.length; i++) {
+        for(let g = i+1; g<arr.length; g++) {
+            let sum = arr[i]+arr[g];
+            if(sum == magicNumber) {
+                console.log(`${arr[i]} ${arr[g]}`);
+            }
+        }
+    }
 }
-solve()
+solve([1, 7, 6, 2, 19, 23],
+    8
+    )
