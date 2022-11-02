ad6b275a18c2cd5f336d4cd5c894a590e51306b0
zlatanovcoding <zlatanovcoding@gmail.com>
2022-11-02 21:29:47 +0200

    Update

---
+++ Programing Basics/PB 6 - Nested Loop/multiplicationTable.js
@@ -0,0 +1,8 @@
+function multiplicationTable() {
+    for(let x =1; x<11; x++) {
+        for(let y = 1; y<11 ; y++) {
+            console.log(`${x} * ${y} = ${x*y}`);
+        }
+    }
+}
+multiplicationTable()
